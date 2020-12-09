68db85abf499b5509a90abbad7b921eb1ea74f7a
VS_code For Mr_CHTO <Mr_CHTO@outlook.com>
2020-12-09 19:05:17 +0800

    1209

--- Quick/Qgit.sh
+++ Quick/Qgit.sh
@@ -11,40 +11,40 @@ if [ $n -eq 0 ]
 then
 	echo "正在拉取all..."
 	cd /usr/local/GitBox/c_box
-	git pull Github master
+	git pull GitHub master
 	cd /usr/local/GitBox/cpp_box
-	git pull Github master
+	git pull GitHub master
 	cd /usr/local/GitBox/java_box
-	git pull Github master
+	git pull GitHub master
 	cd /usr/local/GitBox/py_box
-	git pull Github master
+	git pull GitHub master
 	cd /usr/local/GitBox/sh_box
-	git pull Github master
+	git pull GitHub master
 elif [ $n -eq 1 ]
 then
 	echo "正在拉取c_box..."
 	cd /usr/local/GitBox/c_box
-	git pull Github master
+	git pull GitHub master
 elif [ $n -eq 2 ]
 then
 	echo "正在拉取cpp_box..."
 	cd /usr/local/GitBox/cpp_box
-	git pull Github master
+	git pull GitHub master
 elif [ $n -eq 3 ]
 then
 	echo "正在拉取java_box..."
 	cd /usr/local/GitBox/java_box
-	git pull Github master
+	git pull GitHub master
 elif [ $n -eq 4 ]                                                                               
 then  
 	echo "正在拉取py_box..."
 	cd /usr/local/GitBox/py_box
-	git pull Github master
+	git pull GitHub master
 elif [ $n -eq 5 ]                                                                               
 then  
 	echo "正在拉取sh_box..."
 	cd /usr/local/GitBox/sh_box
-	git pull Github master
+	git pull GitHub master
 else
 	echo "错误，请重试！"
 fi
