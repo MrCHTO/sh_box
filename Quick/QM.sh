05ce3c5cc1e3149a4870ad5330eeb6b0b2fa67c8
VS_code For Mr_CHTO <Mr_CHTO@outlook.com>
2020-12-09 20:12:17 +0800

    1209vs

--- Quick/QM.sh
+++ Quick/QM.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
-echo -e "序号\t类型\t用户名\t备注"
-echo -e "1\t本地\tROOT\t管理员"
-echo -e "2\t本地\tUSER\t用户"
-echo -e "3\t本地\tWORKPRESSUSER\tworkpress管理员"
-echo -e "4\t本地\tHIVEUSER\thive管理员"
-echo -e "5\tRDS\tROOT\t管理员"
-echo -e "6\tRDS\tUSER\t用户"
+echo -e "|序号\t|类型\t|用户名\t|备注"
+echo -e "|1\t|本地\t|ROOT\t\t|管理员"
+echo -e "|2\t|本地\t|USER\t\t|用户"
+echo -e "|3\t|本地\t|WORKPRESSUSER\t\t|workpress管理员"
+echo -e "|4\t|本地\t|HIVEUSER\t\t|hive管理员"
+echo -e "|5\t|RDS\t|RDS_ROOT\t\t|管理员"
+echo -e "|6\t|RDS\t|RDS_USER\t\t|用户"
+read n
+if [ $n - eq 1 ]; then
+    echo "正在进入..."
+    mysql -u root -p
+elif [ $n -eq 2 ]; then
+    echo "正在进入..."
+    mysql -u user -p
+elif [ $n -eq 3 ]; then
+    echo "正在进入..."
+    mysql -u workpressuser -p
+elif [ $n -eq 4 ]; then
+    echo "正在进入..."
+    mysql -u hiveuser -p
+elif [ $n -eq 5 ]; then
+    echo "正在进入..."
+    mysql -h rm-2ze2pr85o2ozj8479po.mysql.rds.aliyuncs.com -P 3306 -u rds_root -p
+elif [ $n -eq 6 ]; then
+    echo "正在进入..."
+    mysql -h rm-2ze2pr85o2ozj8479po.mysql.rds.aliyuncs.com -P 3306 -u rds_user -p
+else
+    echo "错误，请重试！"
+fi
