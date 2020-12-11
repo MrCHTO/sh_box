88a514505c75ad0c31d452d72d6dca863bccfc16
VS_code For Mr_CHTO <Mr_CHTO@outlook.com>
2020-12-11 19:23:18 +0800

    1211VS

--- Quick/Qg.sh
+++ Quick/Qg.sh
@@ -2,58 +2,70 @@
 echo "-----------------------------------------"
 echo -e "| 序号\t| 仓库\t\t| 备注\t\t|"
 echo "-----------------------------------------"
-echo -e "| 1\t| 全部\t\t| 所有仓库\t|"
-echo -e "| 2\t| C_BOX\t\t| C仓库\t\t|"
-echo -e "| 3\t| CPP_BOX\t| C++仓库\t|"
-echo -e "| 4\t| JAVA_BOX\t| JAVA仓库\t|"
-echo -e "| 5\t| PY_BOX\t| PYTHON仓库\t|"
-echo -e "| 6\t| SH_BOX\t| SHELL仓库\t|"
+echo -e "| 0\t| 全部\t\t| 所有仓库\t|"
+echo -e "| 1\t| C_BOX\t\t| C仓库\t\t|"
+echo -e "| 2\t| CPP_BOX\t| C++仓库\t|"
+echo -e "| 3\t| JAVA_BOX\t| JAVA仓库\t|"
+echo -e "| 4\t| PY_BOX\t| PYTHON仓库\t|"
+echo -e "| 5\t| SH_BOX\t| SHELL仓库\t|"
 echo "-----------------------------------------"
 read n
 if [ $n -eq 1 ]; then
 	echo "-----------------------------------------"
-	echo -e "| 1\t| 全部\t\t| 所有仓库\t|"
+	echo -e "| 1\t| C_BOX\t\t| C仓库\t\t|"
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/c_box
-	git pull GitHub master
+	git pull GitHub master:master
+	echo "-----------------------------------------"
+	echo -e "| 2\t| CPP_BOX\t| C++仓库\t|"
+	echo "-----------------------------------------"
 	cd /usr/local/GitBox/cpp_box
-	git pull GitHub master
+	git pull GitHub master:master
+	echo "-----------------------------------------"
+	echo -e "| 3\t| JAVA_BOX\t| JAVA仓库\t|"
+	echo "-----------------------------------------"
 	cd /usr/local/GitBox/java_box
-	git pull GitHub master
+	git pull GitHub master:master
+	echo "-----------------------------------------"
+	echo -e "| 4\t| PY_BOX\t| PYTHON仓库\t|"
+	echo "-----------------------------------------"
 	cd /usr/local/GitBox/py_box
-	git pull GitHub master
+	git pull GitHub master:master
+	echo "-----------------------------------------"
+	echo -e "| 5\t| SH_BOX\t| SHELL仓库\t|"
+	echo "-----------------------------------------"
 	cd /usr/local/GitBox/sh_box
-	git pull GitHub master
+	git pull GitHub master:master
 elif [ $n -eq 2 ]; then
 	echo "-----------------------------------------"
-	echo -e "| 2\t| C_BOX\t\t| C仓库\t\t|"
+	echo -e "| 1\t| C_BOX\t\t| C仓库\t\t|"
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/c_box
-	git pull GitHub master
+	git pull GitHub master:master
 elif [ $n -eq 3 ]; then
 	echo "-----------------------------------------"
-	echo -e "| 3\t| CPP_BOX\t| C++仓库\t|"
+	echo -e "| 2\t| CPP_BOX\t| C++仓库\t|"
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/cpp_box
-	git pull GitHub master
+	git pull GitHub master:master
 elif [ $n -eq 4 ]; then
 	echo "-----------------------------------------"
-	echo -e "| 4\t| JAVA_BOX\t| JAVA仓库\t|"
+	echo -e "| 3\t| JAVA_BOX\t| JAVA仓库\t|"
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/java_box
-	git pull GitHub master
+	git pull GitHub master:master
 elif [ $n -eq 5 ]; then
 	echo "-----------------------------------------"
-	echo -e "| 5\t| PY_BOX\t| PYTHON仓库\t|"
+	echo -e "| 4\t| PY_BOX\t| PYTHON仓库\t|"
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/py_box
-	git pull GitHub master
+	git pull GitHub master:master
 elif [ $n -eq 6 ]; then
 	echo "-----------------------------------------"
-	echo -e "| 6\t| SH_BOX\t| SHELL仓库\t|"
+	echo -e "| 5\t| SH_BOX\t| SHELL仓库\t|"
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/sh_box
-	git pull GitHub master
+	git pull GitHub master:master
 else
 	echo "错误，请重试！"
 fi
