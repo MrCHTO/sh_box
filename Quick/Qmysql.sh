5357ef948304a83065e4d49e0fe09fba201d5c2a
VS_code For Mr_CHTO <Mr_CHTO@outlook.com>
2020-12-11 17:15:32 +0800

    1211

--- Quick/Qmysql.sh
+++ Quick/Qmysql.sh
@@ -1,50 +1,32 @@
 #!/bin/bash
-echo "请-选-择-数-据-库"
-echo "(1)本地数据库_ROOT"
-echo "(2)本地数据库_USER"
-echo "(3)RDS-数据库_ROOT"
-echo "(4)RDS-数据库_USER"
+echo "---------------------------------------------------------"
+echo -e "| 序号\t| 类型\t| 用户名\t| 备注\t\t\t|"
+echo -e "| 1\t| 本地\t| ROOT\t\t| 管理员\t\t|"
+echo -e "| 2\t| 本地\t| USER\t\t| 用户\t\t\t|"
+echo -e "| 3\t| 本地\t| WORKPRESSUSER\t| workpress管理员\t|"
+echo -e "| 4\t| 本地\t| HIVEUSER\t| hive管理员\t\t|"
+echo -e "| 5\t| RDS\t| RDS_ROOT\t| 管理员\t\t|"
+echo -e "| 6\t| RDS\t| RDS_USER\t| 用户\t\t\t|"
+echo "---------------------------------------------------------"
 read n
 if [ $n -eq 1 ]; then
-	f=1
-	echo "正在进入本地数据库_ROOT..."
-	sh /usr/local/mysql/support-files/mysql.server start
-	mysql -u root -p
+    echo -e "| 1\t| 本地\t| ROOT\t\t| 管理员\t\t|"
+    mysql -u root -p
 elif [ $n -eq 2 ]; then
-	f=1
-	echo "正在进入本地数据库_USER..."
-	sh /usr/local/mysql/support-files/mysql.server start
-	mysql -u user -p
+    echo -e "| 2\t| 本地\t| USER\t\t| 用户\t\t\t|"
+    mysql -u user -p
 elif [ $n -eq 3 ]; then
-	echo "正在进入RDS-数据库_ROOT..."
-	mysql -h rm-2ze2pr85o2ozj8479po.mysql.rds.aliyuncs.com -P 3306 -u rds_root -p
+    echo -e "| 3\t| 本地\t| WORKPRESSUSER\t| workpress管理员\t|"
+    mysql -u workpressuser -p
 elif [ $n -eq 4 ]; then
-	echo "正在进入RDS-数据库_USER..."
-	mysql -h rm-2ze2pr85o2ozj8479po.mysql.rds.aliyuncs.com -P 3306 -u rds_user -p
+    echo -e "| 4\t| 本地\t| HIVEUSER\t| hive管理员\t\t|"
+    mysql -u hiveuser -p
+elif [ $n -eq 5 ]; then
+    echo -e "| 5\t| RDS\t| RDS_ROOT\t| 管理员\t\t|"
+    mysql -h rm-2ze2pr85o2ozj8479po.mysql.rds.aliyuncs.com -P 3306 -u rds_root -p
+elif [ $n -eq 6 ]; then
+    echo -e "| 6\t| RDS\t| RDS_USER\t| 用户\t\t\t|"
+    mysql -h rm-2ze2pr85o2ozj8479po.mysql.rds.aliyuncs.com -P 3306 -u rds_user -p
 else
-	echo "错误，请重试！"
-fi
-
-if [ $f -eq 1 ]; then
-	echo "是否关闭本地MYSQL服务？[1/0]"
-	read a
-	if [ $a -eq 1 ]; then
-		sh /usr/local/mysql/support-files/mysql.server stop
-	elif [ $a -eq 0 ]; then
-		echo "MYSQL服务后台运行中..."
-	else
-		echo "MYSQL服务后台运行中..."
-	fi
-elif [ $f -eq 1 ]; then
-	echo "是否关闭本地MYSQL服务？[1/0]"
-	read a
-	if [ $a -eq 1 ]; then
-		sh /usr/local/mysql/support-files/mysql.server stop
-	elif [ $a -eq 0 ]; then
-		echo "MYSQL服务后台运行中..."
-	else
-		echo "MYSQL服务后台运行中..."
-	fi
-else
-	echo "MYSQL服务后台运行中..."
+    echo "错误，请重试！"
 fi
