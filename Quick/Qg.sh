c7a147f3510a37ab3f4866eb73f444f4f09a91bc
VS_code For Mr_CHTO <Mr_CHTO@outlook.com>
2020-12-11 19:24:32 +0800

    1211VS

--- Quick/Qg.sh
+++ Quick/Qg.sh
@@ -10,7 +10,7 @@ echo -e "| 4\t| PY_BOX\t| PYTHON仓库\t|"
 echo -e "| 5\t| SH_BOX\t| SHELL仓库\t|"
 echo "-----------------------------------------"
 read n
-if [ $n -eq 1 ]; then
+if [ $n -eq 0 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 1\t| C_BOX\t\t| C仓库\t\t|"
 	echo "-----------------------------------------"
@@ -36,31 +36,31 @@ if [ $n -eq 1 ]; then
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/sh_box
 	git pull GitHub master:master
-elif [ $n -eq 2 ]; then
+elif [ $n -eq 1 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 1\t| C_BOX\t\t| C仓库\t\t|"
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/c_box
 	git pull GitHub master:master
-elif [ $n -eq 3 ]; then
+elif [ $n -eq 2 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 2\t| CPP_BOX\t| C++仓库\t|"
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/cpp_box
 	git pull GitHub master:master
-elif [ $n -eq 4 ]; then
+elif [ $n -eq 3 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 3\t| JAVA_BOX\t| JAVA仓库\t|"
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/java_box
 	git pull GitHub master:master
-elif [ $n -eq 5 ]; then
+elif [ $n -eq 4 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 4\t| PY_BOX\t| PYTHON仓库\t|"
 	echo "-----------------------------------------"
 	cd /usr/local/GitBox/py_box
 	git pull GitHub master:master
-elif [ $n -eq 6 ]; then
+elif [ $n -eq 5 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 5\t| SH_BOX\t| SHELL仓库\t|"
 	echo "-----------------------------------------"
