3effbc90a8f78606e818295db3ced9a24f3128a9
Mr_CHTO <Mr_CHTO@outlook.com>
2021-05-08 15:51:28 +0800

    0508VS

--- Mybin/Qg.sh
+++ Mybin/Qg.sh
@@ -14,57 +14,57 @@ if [ $n -eq 0 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 1\t| C_BOX\t\t| C仓库\t\t|"
 	echo "-----------------------------------------"
-	cd /usr/local/GitBox/c_box
+	cd /usr/GitBox/c_box
 	git pull origin master:master
 	echo "-----------------------------------------"
 	echo -e "| 2\t| CPP_BOX\t| C++仓库\t|"
 	echo "-----------------------------------------"
-	cd /usr/local/GitBox/cpp_box
+	cd /usr/GitBox/cpp_box
 	git pull origin master:master
 	echo "-----------------------------------------"
 	echo -e "| 3\t| JAVA_BOX\t| JAVA仓库\t|"
 	echo "-----------------------------------------"
-	cd /usr/local/GitBox/java_box
+	cd /usr/GitBox/java_box
 	git pull origin master:master
 	echo "-----------------------------------------"
 	echo -e "| 4\t| PY_BOX\t| PYTHON仓库\t|"
 	echo "-----------------------------------------"
-	cd /usr/local/GitBox/py_box
+	cd /usr/GitBox/py_box
 	git pull origin master:master
 	echo "-----------------------------------------"
 	echo -e "| 5\t| SH_BOX\t| SHELL仓库\t|"
 	echo "-----------------------------------------"
-	cd /usr/local/GitBox/sh_box
+	cd /usr/GitBox/sh_box
 	git pull origin master:master
 elif [ $n -eq 1 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 1\t| C_BOX\t\t| C仓库\t\t|"
 	echo "-----------------------------------------"
-	cd /usr/local/GitBox/c_box
+	cd /usr/GitBox/c_box
 	git pull origin master:master
 elif [ $n -eq 2 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 2\t| CPP_BOX\t| C++仓库\t|"
 	echo "-----------------------------------------"
-	cd /usr/local/GitBox/cpp_box
+	cd /usr/GitBox/cpp_box
 	git pull origin master:master
 elif [ $n -eq 3 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 3\t| JAVA_BOX\t| JAVA仓库\t|"
 	echo "-----------------------------------------"
-	cd /usr/local/GitBox/java_box
+	cd /usr/GitBox/java_box
 	git pull origin master:master
 elif [ $n -eq 4 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 4\t| PY_BOX\t| PYTHON仓库\t|"
 	echo "-----------------------------------------"
-	cd /usr/local/GitBox/py_box
+	cd /usr/GitBox/py_box
 	git pull origin master:master
 elif [ $n -eq 5 ]; then
 	echo "-----------------------------------------"
 	echo -e "| 5\t| SH_BOX\t| SHELL仓库\t|"
 	echo "-----------------------------------------"
-	cd /usr/local/GitBox/sh_box
+	cd /usr/GitBox/sh_box
 	git pull origin master:master
 else
 	echo "错误，请重试！"
